4b61560a6ad720d5f422f98ce27645a76e588dac
samuelg <sam2018gs@gmail.com>
2025-03-30 20:11:14 -0300

    Cadastrando cortes

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { UpdateUserController } from "./controllers/user/UpdateUserController";
+import { CreateHaircutController } from "./controllers/haircut/CreateHaircutController";
 
 const router = Router();
 
@@ -12,5 +13,8 @@ router.post("/user", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
 router.get("/me", isAuthenticated, new DetailUserController().handle);
 router.put("/user/update", isAuthenticated, new UpdateUserController().handle);
+
+//Rotas corte
+router.post("/haircut", isAuthenticated, new CreateHaircutController().handle);
 
 export { router };
